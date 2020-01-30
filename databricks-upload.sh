82d9feb4f4175bea7959c95bf72fbd119dc62a9b
slixperi <rmccarth@andrew.cmu.edu>
2020-01-30 14:49:09 -0500

    updated with databricks-upload dev script and s3-pull updates

---
+++ databricks-upload.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+
+#script takes a file from a python transformation and uploads it to the databricks DBFS via the DBFS cli
+
+#databricks cli installation instructions:
+#sudo pip3 install databricks-cli
+
+FILES="data/"
+
+for file in $FILES
+do
+	
+done
