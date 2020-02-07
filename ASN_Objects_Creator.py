d172f43f4e543c3d5e3e3e08a1bf8e9d19798c22
rgsingh01 <rgsingh@andrew.cmu.edu>
2020-02-06 20:18:15 -0500

    added small code to serialize ASN object list and put it in redis

--- ASN_Objects_Creator.py
+++ ASN_Objects_Creator.py
@@ -4,12 +4,16 @@
 Created on Mon Feb  3 14:08:22 2020
 
 @author: jacksonbrietzke
+@author: rajatsingh
 """
 
 import pandas as pd
 import numpy as np
 import re
 import csv
+import argparse
+import redis
+import pickle
 
 class Event:
     def __init__(self,event_id, ip_address, confidence, hostility, reputation_rating):
@@ -29,46 +33,57 @@ class Event:
             except:
                 self.reputation_rating = 0
             self.score = self.create_score()
-        except Exception as e: 
+        except Exception as e:
             print(e)
             print(confidence, hostility, reputation_rating)
-        
+
     def create_score(self):
         try:
             return self.confidence + self.reputation_rating
-        except Exception as e: 
+        except Exception as e:
             print(e)
-    
+
 class ASN:
     def __init__(self):
         self.as_number = 'TBD'
         self.events_list = []
         self.score = 0
-    
+
     def __init__(self, as_number):
         try:
             self.as_number = int(float(as_number))
-        except Exception as e: 
+        except Exception as e:
             print(e)
             self.as_number = 'Undefined'
         self.events_list = []
         self.score = 0
-    
+
     def create_score(self):
         for x in self.events_list:
             try:
                 self.score += x.score
-            except Exception as e: 
+            except Exception as e:
                 print(e)
-    
-    
+
+
+def print_asn_obj(asn_obj):
+    pass
+
 def main():
+
+    r = redis.Redis(host='localhost', port=6379, db=0)
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--infile', dest='infile', help='csv file path')
+    parser.add_argument('--outfile', dest='outfile', help='output ASN/Score file path')
+
+    args = parser.parse_args()
+
     print("Creating ASN Objects")
     asn_objects = []
     for x in range(0,500000):
         asn_objects.append(ASN(x))
-        
-    master_df = pd.read_csv('Full/Output/CLEANED.csv', low_memory=False)
+
+    master_df = pd.read_csv(args.infile, low_memory=False)
     master_df.sort_values(by='ASN', inplace=True)
     for x in range(len(master_df.index)):
         temp_event = Event(master_df['ID'][x], master_df['IP_Address'][x],
@@ -76,15 +91,22 @@ def main():
                            master_df['Reputation_Rating'][x])
         asn_objects[master_df['ASN'][x]].events_list.append(temp_event)
 
-    with open('Full/Output/ASN_Scores.csv', 'w') as file:
-      
+    pickled_asn_objs = pickle.dumps(asn_objects)
+    r.set('asn_objects', pickled_asn_objs)
+
+    #prints long ass string
+    #print(r.get('asn_objects'))
+
+
+    with open(args.outfile, 'w') as file:
+
        writer = csv.writer(file)
        writer.writerow(['ASN', 'Score'])
        for x in asn_objects:
            x.create_score()
            writer.writerow([x.as_number, x.score])
-        
-        
-    
+
+
+
 if __name__ == "__main__":
     main()
