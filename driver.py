41aba639a5f3fbbb81ec39249367cbafdc1b19e0
slixperi <rmccarth@andrew.cmu.edu>
2020-05-03 16:53:30 -0400

    set interpreter

--- driver.py
+++ driver.py
@@ -1,28 +1,10 @@
-"""
-Created on Tue Feb 11 09:52:06 2020
-
-@author: jacksonbrietzke
-@author: rajsingh
-
-"""
-
-""" This is the driver program for all of the Python scripts; it calls all of the necessary
-    functions to aggregate, clean, and reformat all of the Deepsight data that is ingested
-    and it also creates the ASN objects and uses them for various analyses
-"""
-
+#!/usr/bin/env python3
 import os
 import time
 import ASN
 import redis
 
-""" The main function of the program; calls ASN library functions for performing the tasks
-    mentioned in the file description above
-
-"""
 def main():
-    """Driver function for ASN Program"""
-    print("inside driver")
     start_time = time.time()
     input_path = 'data/'
     output_path = 'master/'
@@ -31,17 +13,8 @@ def main():
     ASN.aggregating_files.creating_files(input_path, output_path)
     ASN.creating_asn_objects.creating_asns(output_path, input_path)
     ASN.creating_asn_objects.stop_redis(redis_instance)
-    print("--- %s seconds ---" % (time.time() - start_time))
 
-
-""" Does some error checking with directories to make sure the program runs properly
-
-Args
--------
-    input_path (str): input path to the data files (Deepsight files)
-    output_path (str): where to write all of the output CSV's to
-"""
-def setup_directories(input_path: str , output_path: str):
+def setup_directories(input_path: str, output_path: str):
     if not os.path.isdir(input_path):
         os.mkdir(input_path)
     if not os.path.isdir(output_path):
@@ -51,14 +24,6 @@ def setup_directories(input_path: str , output_path: str):
     if not os.path.exists(input_path + 'geolite_ordered.csv'):
         ASN.ordering_geolite.cleaning_geolite(input_path)
 
-
-""" Starts a redis instance and checks whether or not this is a rolling ingest.
-    If it is; it will increment the master version that should be outputted (1..n)
-
-Returns
--------
-    redis_instance (redis.StrictRedis): the connection to the redis database
-"""
 def setup_redis() -> redis.StrictRedis:
     redis_instance = ASN.creating_asn_objects.start_redis()
     if os.path.exists('master/serialized_before'):
@@ -67,5 +32,4 @@ def setup_redis() -> redis.StrictRedis:
         redis_instance.set('master_version', 1)
     return redis_instance
 
-print("inside driver.py")
 main()
