ec1f738880f5dfa8fec308c41c02d880aa707e0f
slixperi <rmccarth@andrew.cmu.edu>
2020-05-03 19:28:32 -0400

    refactor imports, combine aggregating_files with creating_asn_objects

--- driver.py
+++ driver.py
@@ -7,6 +7,7 @@ import redis
 
 
 def main():
+    """Driver function for ASN Program"""
     start_time = time.time()
     input_path = 'data/'
     output_path = 'master/'
@@ -15,8 +16,16 @@ def main():
     creating_asn_objects.creating_files(input_path, output_path)
     creating_asn_objects.creating_asns(output_path, input_path)
     creating_asn_objects.stop_redis(redis_instance)
+    print("--- %s seconds ---" % (time.time() - start_time))
 
-def setup_directories(input_path: str, output_path: str):
+
+""" Does some error checking with directories to make sure the program runs properly
+Args
+-------
+    input_path (str): input path to the data files (Deepsight files)
+    output_path (str): where to write all of the output CSV's to
+"""
+def setup_directories(input_path: str , output_path: str):
     if not os.path.isdir(input_path):
         os.mkdir(input_path)
     if not os.path.isdir(output_path):
@@ -26,6 +35,13 @@ def setup_directories(input_path: str, output_path: str):
     if not os.path.exists(input_path + 'geolite_ordered.csv'):
         ordering_geolite.cleaning_geolite(input_path)
 
+
+""" Starts a redis instance and checks whether or not this is a rolling ingest.
+    If it is; it will increment the master version that should be outputted (1..n)
+Returns
+-------
+    redis_instance (redis.StrictRedis): the connection to the redis database
+"""
 def setup_redis() -> redis.StrictRedis:
     redis_instance = creating_asn_objects.start_redis()
     if os.path.exists('master/serialized_before'):
@@ -33,5 +49,8 @@ def setup_redis() -> redis.StrictRedis:
     else:
         redis_instance.set('master_version', 1)
     return redis_instance
+
+
+main()
 
 main()
