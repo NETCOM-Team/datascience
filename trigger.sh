c89e18725fd5e9e1565540342eabbca6feb24990
slixperi <rmccarth@andrew.cmu.edu>
2020-05-03 15:58:25 -0400

    debug

--- trigger.sh
+++ trigger.sh
@@ -7,9 +7,8 @@ do
     #watches the data directory for updates from aws s3 sync in the s3-job.sh
     inotifywait -e create data/
     echo "inotify exited, moving onto calling driver.py"
-    python -c 'print("hello world")'
     #when inotifywait triggers, call our driver.py to compile updates in data/
-    ./driver.py && echo "this line calls driver directly"
+    python driver.py && echo "printing b/c we are executing the most important line"
     echo "driver exited"
     #driver.py outputs a MASTER.csv file of compiled threat intel feeds to the master/ directory
     aws s3 cp master/MASTER.csv s3://$BUCKET_NAME
