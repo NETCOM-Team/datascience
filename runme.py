4fd6517692bb56c65a3e2b354721199c9b71be07
slixperi <rmccarth@andrew.cmu.edu>
2020-05-03 18:17:27 -0400

    commit

--- runme.py
+++ runme.py
@@ -10,11 +10,9 @@ def main():
     start_time = time.time()
     input_path = 'data/'
     output_path = 'master/'
-    redis_instance = setup_redis()
     setup_directories(input_path, output_path)
     ASN.aggregating_files.creating_files(input_path, output_path)
     ASN.creating_asn_objects.creating_asns(output_path, input_path)
-    ASN.creating_asn_objects.stop_redis(redis_instance)
 
 def setup_directories(input_path: str, output_path: str):
     if not os.path.isdir(input_path):
@@ -26,12 +24,5 @@ def setup_directories(input_path: str, output_path: str):
     if not os.path.exists(input_path + 'geolite_ordered.csv'):
         ASN.ordering_geolite.cleaning_geolite(input_path)
 
-def setup_redis() -> redis.StrictRedis:
-    redis_instance = ASN.creating_asn_objects.start_redis()
-    if os.path.exists('master/serialized_before'):
-        redis_instance.incr('master_version')
-    else:
-        redis_instance.set('master_version', 1)
-    return redis_instance
 
 main()
