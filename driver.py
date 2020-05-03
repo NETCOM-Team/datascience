4d038dd62cba60324faedce2570cf22987174e59
slixperi <rmccarth@andrew.cmu.edu>
2020-05-03 18:47:34 -0400

    commit

--- driver.py
+++ driver.py
@@ -12,9 +12,9 @@ def main():
     output_path = 'master/'
     redis_instance = setup_redis()
     setup_directories(input_path, output_path)
-    ASN.aggregating_files.creating_files(input_path, output_path)
-    ASN.creating_asn_objects.creating_asns(output_path, input_path)
-    ASN.creating_asn_objects.stop_redis(redis_instance)
+    aggregating_files.creating_files(input_path, output_path)
+    creating_asn_objects.creating_asns(output_path, input_path)
+    creating_asn_objects.stop_redis(redis_instance)
 
 def setup_directories(input_path: str, output_path: str):
     if not os.path.isdir(input_path):
@@ -22,12 +22,12 @@ def setup_directories(input_path: str, output_path: str):
     if not os.path.isdir(output_path):
         os.mkdir(output_path)
     if not os.path.exists(output_path + 'geolite_lookup.csv'):
-        ASN.creating_lookups.create_geolite_lookup(input_path, output_path)
+        creating_lookups.create_geolite_lookup(input_path, output_path)
     if not os.path.exists(input_path + 'geolite_ordered.csv'):
-        ASN.ordering_geolite.cleaning_geolite(input_path)
+        ordering_geolite.cleaning_geolite(input_path)
 
 def setup_redis() -> redis.StrictRedis:
-    redis_instance = ASN.creating_asn_objects.start_redis()
+    redis_instance = creating_asn_objects.start_redis()
     if os.path.exists('master/serialized_before'):
         redis_instance.incr('master_version')
     else:
